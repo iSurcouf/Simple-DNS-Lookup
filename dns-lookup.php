22c9c891abb23a5aa2be1c4eee88f4d2ff74298f
iSurcouf <me@surcouf.me>
2016-01-26 19:53:30 +0100

    Update dns-lookup.php

--- dns-lookup.php
+++ dns-lookup.php
@@ -15,7 +15,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="jumbotron-narrow.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
