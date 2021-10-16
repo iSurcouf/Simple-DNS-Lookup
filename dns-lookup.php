7f22b8ba6cd3c474ecba4f1d63e3000a04987938
iSurcouf <surcouf@pm.me>
2021-10-16 16:44:49 +0200

    Update dns-lookup.php

--- dns-lookup.php
+++ dns-lookup.php
@@ -4,21 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <meta name="description" content="">
         <meta name="author" content="HQWEB">
-        <link rel="icon" href="https://www.hqweb.com/images/favicon.ico">
 
         <title>Simple DNS Lookup</title>
 
         <!-- Custom styles for this template -->
         <link href="style.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body>
         <div class="container">
